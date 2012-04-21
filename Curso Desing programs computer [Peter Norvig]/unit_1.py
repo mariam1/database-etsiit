5caf50b9cc13a1de1b95c88c46f0f060eb13481b
andrewduque15@gmail.com <andrewduque15@gmail.com@0105baee-7545-d921-cd12-6ace9199ab25>
2012-04-21 22:27:28 +0000

    python

---
+++ Curso Desing programs computer [Peter Norvig]/unit_1.py
@@ -0,0 +1,13 @@
+## WRITE A POKER PROGRAM ##
+## ?? --> problem(understand) --> spec(specify)--->code(design)
+## hand_card; rank suit;poker(hands)->hand; hand_rank; hand->2 pair; n-kind(tipo)
+## straight;flush;
+
+##poker function
+
+#def poker(hands):
+#  "Return the best hand: poker([hand,..]) => "hand"
+#  return max
+
+print max([3,4,5,0]),max([3,4,-5,0,key=abs)
+
