96d1c8ee3cbeff4e5f227afe0c37f317fcb99a50
andrewduque15@gmail.com <andrewduque15@gmail.com@0105baee-7545-d921-cd12-6ace9199ab25>
2012-04-21 22:27:28 +0000

    python

---
+++ trunk/Curso Desing programs computer [Peter Norvig]/list_methods.py
@@ -0,0 +1,12 @@
+udacity_profs = ['dave','sebastian','peter','steve','wes','dave']
+print udacity_profs
+
+steve_count = udacity_profs.count('steve')
+print steve_count
+
+sorted_profs = udacity_profs.sort()
+print sorted_profs
+print udacity_profs
+
+udacity_profs.reverse()
+print udacity_profs
